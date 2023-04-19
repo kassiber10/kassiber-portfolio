c2206d22beaa1e2eb6766bef184f767bf87915c6
kassiber10 <almailreciever@gmail.com>
2023-04-19 11:05:14 +0200

    Cleaned up router.js

--- src/router.js
+++ src/router.js
@@ -4,11 +4,6 @@ import ProjectsIndex from "./pages/ProjectsPage/ProjectsIndex.vue";
 import ProjectDetails from "./pages/ProjectsPage/ProjectDetails.vue"
 import LandingPage from "./pages/LandingPage.vue";
 import ContactIndex from "./pages/ContactPage/ContactIndex.vue";
-// import PortfolioPage from "./pages/PortfolioPage/PortfolioIndex.vue";
-// import GaleriePage from "./pages/GaleriePage/GalerieIndex.vue";
-// import PricesPage from "./pages/PricesPage/PricesIndex.vue";
-// import AboutPage from "./pages/AboutPage/AboutIndex.vue";
-// import ContactPage from "./pages/ContactPage/ContactIndex.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -17,11 +12,6 @@ const router = createRouter({
     { path: "/project/:id", name: "project", component: ProjectDetails },
     { path: "/", name: "root", component: LandingPage },
     { path: "/contact", name: "contact", component: ContactIndex },
-    // { path: "/portfolio", component: PortfolioPage },
-    // { path: "/galeries", component: GaleriePage },
-    // { path: "/prices", component: PricesPage },
-    // { path: "/about", component: AboutPage },
-    // { path: "/contact", component: ContactPage },
   ],
 });
 
